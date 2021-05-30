0bd7908b0449a6691d220eb909c4a6d71ad6e8c8
Deivid Urrego <deivid_199615@hotmail.com>
2021-05-29 23:00:01 -0500

    Diseño y configuración de registrar especial

--- src/app/pages/consulta-especial/consulta-especial.component.ts
+++ src/app/pages/consulta-especial/consulta-especial.component.ts
@@ -6,6 +6,16 @@ import { Paciente } from '../../models/paciente';
 import { PacienteService } from '../../services/paciente.service';
 import { Medico } from '../../models/medico';
 import { MedicoService } from '../../services/medico.service';
+import { Examen } from '../../models/examen';
+import { Especialidad } from '../../models/especialidad';
+import { DetalleConsulta } from '../../models/detalleConsulta';
+import { ExamenService } from '../../services/examen.service';
+import { EspecialidadService } from '../../services/especialidad.service';
+import { ConsultaService } from '../../services/consulta.service';
+import { MatSelectChange } from '@angular/material/select';
+import { Consulta } from '../../models/consulta';
+import * as moment from 'moment';
+import { ConsultaListaExamenDTO } from '../../dto/consultaListaExamenDTO';
 
 @Component({
   selector: 'app-consulta-especial',
@@ -16,6 +26,23 @@ export class ConsultaEspecialComponent implements OnInit {
 
   pacientes: Paciente[] = [];
   medicos: Medico[] = [];
+  especialidades: Especialidad[] = [];
+  examenes: Examen[] = [];
+
+  detalleConsulta: DetalleConsulta[] = [];
+  examenesSeleccionados: Examen[] = [];
+
+  diagnostico: string = '';
+  tratamiento: string = '';
+  mensaje: string = '';
+
+  fechaSeleccionada: Date = new Date();
+  maxFecha: Date = new Date();
+
+  pacienteSeleccionado!: Paciente;
+  medicoSeleccionado!: Medico;
+  especialidadSeleccionada!: Especialidad;
+  examenSeleccionado!: Examen;
 
   myControlPaciente: FormControl = new FormControl();
   myControlMedico: FormControl = new FormControl();
@@ -33,13 +60,19 @@ export class ConsultaEspecialComponent implements OnInit {
   pacientesFiltrados$!: Observable<Paciente[]>;
   medicosFiltrados$!: Observable<Medico[]>;
 
-  constructor( private pacienteService: PacienteService,
-                private medicoService: MedicoService ) { }
+  constructor( 
+    private pacienteService: PacienteService,
+    private medicoService: MedicoService,
+    private examenService: ExamenService,
+    private especialidadService: EspecialidadService,
+    private consultaService: ConsultaService ) { }
 
   ngOnInit(): void {
 
     this.listarPacientes();
     this.listarMedicos();
+    this.listarEspecialidad();
+    this.listarExamenes();
 
     this.pacientesFiltrados$ = this.myControlPaciente.valueChanges
       .pipe(
@@ -102,8 +135,98 @@ export class ConsultaEspecialComponent implements OnInit {
     
   }
 
+  listarEspecialidad() {
+    this.especialidadService.listar().subscribe(data => {
+      this.especialidades = data;
+    });
+  }
+
+  listarExamenes() {
+    this.examenService.listar().subscribe(data => {
+      this.examenes = data;
+    });
+  }
+
+  seleccionarEsp( e: MatSelectChange ) {
+    console.log(e.value);
+  }
+
+  agregar() {
+    if(this.diagnostico != '' && this.tratamiento != '') {
+      let det = new DetalleConsulta();
+      det.diagnostico = this.diagnostico;
+      det.tratamiento = this.tratamiento;
+      this.detalleConsulta.push(det);
+      this.diagnostico = '';
+      this.tratamiento = '';
+    }else{
+      this.examenService.openSnackBar("Debe agregar un diagnóstico y un tratamiento");
+    }
+  }
+
+  removerDiagnostico( index: number ){
+    this.detalleConsulta.splice(index, 1);
+  }
+
+  removerExamen( index: number ){
+    this.examenesSeleccionados.splice(index, 1);
+  }
+
+  estadoBotonRegistrar(){
+    return (this.detalleConsulta.length === 0 || this.especialidadSeleccionada === null || this.medicoSeleccionado === null || this.pacienteSeleccionado === null )
+  }
+
+  agregarExamen() {
+    if(this.examenSeleccionado){
+      let encontrado = this.examenesSeleccionados.some( examen => examen.idExamen === this.examenSeleccionado.idExamen );
+
+      if(encontrado){
+        this.examenService.openSnackBar('El examen ya se encuentra en la lista');
+      }else{
+        this.examenesSeleccionados.push(this.examenSeleccionado);
+        this.examenSeleccionado = null!;
+      }
+    }
+  }
+
   aceptar() {
-    console.log(this.form.value);
+    if(!this.estadoBotonRegistrar()){
+
+      let consulta = new Consulta();
+      consulta.medico = this.form.value['medico'];
+      consulta.paciente = this.form.value['paciente'];
+      consulta.especialidad = this.form.value['especialidad'];
+      consulta.detalleConsulta = this.detalleConsulta;
+      consulta.numConsultorio = "C1";
+      consulta.fecha = moment(this.fechaSeleccionada).format('YYYY-MM-DDTHH:mm:ss');
+      // let tzoffset = (new Date()).getTimezoneOffset() * 60000;
+      // let localISOTime = (new Date(this.fechaSeleccionada.getTime() - tzoffset)).toISOString();
+      let dto: ConsultaListaExamenDTO = new ConsultaListaExamenDTO();
+      dto.consulta = consulta;
+      dto.listaExamenes = this.examenesSeleccionados;
+
+      this.consultaService.registrarTransaccion(dto).subscribe( () => {
+        this.consultaService.openSnackBar('Se registro la consulta correctamente');
+        setTimeout(() => {
+          this.limpiarControler();
+        }, 2000);
+      });
+    }
+  }
+
+  limpiarControler() {
+    this.detalleConsulta = [];
+    this.examenesSeleccionados = [];
+    this.diagnostico = '';
+    this.tratamiento = '';
+    this.pacienteSeleccionado = null!;
+    this.especialidadSeleccionada = null!;
+    this.medicoSeleccionado = null!;
+    this.examenesSeleccionados = null!;
+    this.fechaSeleccionada = new Date();
+
+    this.myControlMedico.reset();
+    this.myControlPaciente.reset();
   }
 
 }
